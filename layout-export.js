78e7b27a9dc018b2a27a02d33e9c4c29e1c9a2ce
cbemstar <67246734+cbemstar@users.noreply.github.com>
2025-07-29 17:01:29 +1200

    Expand layout tool features

--- layout-export.js
+++ layout-export.js
@@ -4,6 +4,16 @@ function canvasToPng(canvas) {
   return canvas.toDataURL('image/png');
 }
 
+function canvasToJpg(canvas, quality = 0.92) {
+  return canvas.toDataURL('image/jpeg', quality);
+}
+
+function canvasToSvg(canvas) {
+  const dataUrl = canvas.toDataURL('image/png');
+  const svg = `<svg xmlns="http://www.w3.org/2000/svg" width="${canvas.width}" height="${canvas.height}"><image href="${dataUrl}" width="100%" height="100%"/></svg>`;
+  return svg;
+}
+
 async function canvasToPdf(canvas) {
   const pdf = await PDFDocument.create();
   const pngData = canvas.toDataURL('image/png');
@@ -14,4 +24,4 @@ async function canvasToPdf(canvas) {
   return pdf.save();
 }
 
-module.exports = { canvasToPng, canvasToPdf };
+module.exports = { canvasToPng, canvasToJpg, canvasToSvg, canvasToPdf };
